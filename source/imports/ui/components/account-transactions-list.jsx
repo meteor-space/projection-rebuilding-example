bf79eb9bdb4e8955fb4076da7d7b0c9961bd6f05
darko-mijic <darko.mijic@gmail.com>
2016-03-26 14:02:38 +0100

    Fix displaying of amount in transaction

--- source/imports/ui/components/account-transactions-list.jsx
+++ source/imports/ui/components/account-transactions-list.jsx
@@ -4,7 +4,7 @@ export default AccountTransactionsList = ({transactions}) => (
   <div>
     <ul>
       {transactions.map(({_id, amount, transactionType}) => (
-        <li key={_id}>{_id}: {transactionType} {amount.amount.amount} {amount.currency.code}</li>
+        <li key={_id}>{_id}: {transactionType} {amount.amount} {amount.currency.code}</li>
       ))}
     </ul>
   </div>
