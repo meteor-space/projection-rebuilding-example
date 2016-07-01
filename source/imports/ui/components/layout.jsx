80bbb75e399261443c9dcdaf1af34e9b05384b2f
darko-mijic <darko.mijic@gmail.com>
2016-07-01 09:41:17 +0200

    Add material-ui npm dependency and use it in the main layout

--- source/imports/ui/components/layout.jsx
+++ source/imports/ui/components/layout.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import BankersCounter from '/source/imports/ui/containers/bankers-counter.jsx';
-import AccountTransactionsList from '/source/imports/ui/containers/account-transactions-list';
-import AccountsList from '/source/imports/ui/containers/accounts-list';
+import {Grid, Row, Col} from 'meteor/jimmiebtlr:react-flexbox-grid';
+import AppBar from 'material-ui/AppBar';
+import BankersCounter from '../containers/bankers-counter.jsx';
+import AccountTransactionsList from '../containers/account-transactions-list';
+import AccountsList from '../containers/accounts-list';
 
-export default Layout = () => (
+const Layout = () => (
   <div>
-    <h1>Space Bank</h1>
-    <hr />
-    <header>
-        <BankersCounter />
-    </header>
-    <div className="main">
-      <AccountTransactionsList />
-    </div>
-    <div className="main">
-      <AccountsList />
-    </div>
+
+    <AppBar title="Space Banking" showMenuIconButton={false}/>
+
+    <Grid>
+      <Row>
+        <Col xs={6}>
+          <BankersCounter />
+          <AccountTransactionsList />
+          <AccountsList />
+        </Col>
+        <Col xs={6}>
+        </Col>
+      </Row>
+    </Grid>
+
   </div>
 );
+
+export default Layout;
--- source/routes.jsx
+++ source/routes.jsx
@@ -3,10 +3,20 @@ import { mount } from 'react-mounter';
 import { Provider } from 'react-redux';
 import Layout from '/source/imports/ui/components/layout.jsx';
 import store from '/source/imports/ui/store';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+const AppbarStyles = getMuiTheme({
+  palette: {
+    accent1Color: '#3dbccd'
+  }
+});
 
 const App = (store) => (
   <Provider store={store}>
-    <Layout />
+    <MuiThemeProvider muiTheme={AppbarStyles}>
+      <Layout />
+    </MuiThemeProvider>
   </Provider>
 );
 
