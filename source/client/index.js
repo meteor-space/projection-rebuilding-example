28f7399e11bd25069502129c4151ed1529b317b1
Rhys Bartels-Waller <rhyslbw@gmail.com>
2016-09-09 14:57:01 +1000

    Add react-tap-event-plugin

--- source/client/index.js
+++ source/client/index.js
@@ -1,7 +1,9 @@
 import App from '../imports/ui/application.js';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 Meteor.startup(function() {
   if (Meteor.isClient) {
+    injectTapEventPlugin();
     App.start();
   }
 });
